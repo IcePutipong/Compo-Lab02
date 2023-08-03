c4cb48eb85931c46a2c92dd05677d23c6d6250a0
Putipong Sailen <tyice789@gmail.com>
2023-08-03 14:58:04 +0700

    Lab04: 1.1-1.6 finish!!

--- Lab02-Putipong/src/views/event/EventRegisterView.vue
+++ Lab02-Putipong/src/views/event/EventRegisterView.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
 import { type EventItem } from '@/type'
 import type { PropType } from 'vue'
+import { useRouter } from 'vue-router';
+import { useMessangeStore } from '@/stores/message';
 
-defineProps({
+const props = defineProps({
     event: {
         type: Object as PropType<EventItem>,
         require: true
     }
 })
 
+const router = useRouter()
+const store = useMessangeStore()
+function register(){
+    store.updateMessage('You are successfully registerd for'+ props.event?.title)
+    setTimeout(() =>{
+        store.resetMessage()
+    },3000)
+    router.push({
+        name: ' event-detail',
+        params:{
+            id: props.event?.id
+        }
+    })
+}
+
 
 </script>
 <template>
         <p> Registration from here </p>
+        <button @click="register"> Register ME </button>
 </template>
