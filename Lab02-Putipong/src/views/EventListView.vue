4e3efefa08c855ad55bdb6476d4c31cdff777487
Putipong Sailen <tyice789@gmail.com>
2023-08-05 12:00:36 +0700

    Lab04: 2.3

--- Lab02-Putipong/src/views/EventListView.vue
+++ Lab02-Putipong/src/views/EventListView.vue
@@ -18,8 +18,12 @@
 import EventCard from '../components/EventCard.vue'
 import type { EventItem } from '@/type'
 import { ref, type Ref, watchEffect, computed } from 'vue'
+import { onBeforeRouteUpdate } from 'vue-router'
 import EventService from '@/services/EventService'
+import NProgress from 'nprogress'
+import { useRouter } from 'vue-router'
 import type { Axios, AxiosResponse } from 'axios'
+import router from '@/router'
 
 
 const events: Ref<Array<EventItem>> = ref([])
@@ -36,12 +40,32 @@ const props = defineProps({
   }
 })
 
-watchEffect(() => {
-  EventService.getEvent(props.size, props.page).then((response: AxiosResponse<EventItem[]>) => {
+
+NProgress.start()
+EventService.getEvent(props.size, props.page).then((response: AxiosResponse<EventItem[]>) => {
     events.value = response.data
     totalEvent.value = response.headers['x-total-count']
+  }).catch(()=>{
+    router.push({ name: 'NetworkError'})
+  }).finally(() =>{
+    NProgress.done()
   })
-})
+
+  onBeforeRouteUpdate((to, from, next) =>{
+    const toPage = Number(to.query.page)
+    NProgress.start()
+    ///????????
+    EventService.getEvent(props.size, toPage).then((response: AxiosResponse<EventItem[]>) =>{
+      events.value = response.data
+      totalEvent.value = response.headers['x-total-count']
+      next()
+    }).catch(() => {
+      next({ name: 'NetworkError'})
+    }).finally(()=>{
+      NProgress.done()
+    })
+  })
+    
 
 const hasNextPage = computed(() => {
   const totalPages = Math.ceil(totalEvent.value / 2)
